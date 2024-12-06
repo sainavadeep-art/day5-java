b6558ae67dd14099ae17331b2fd855cff59dbb92
sainavadeep-art <chittoorsainavadeep1135@gmail.com>
2024-12-06 11:17:07 +0530

    Add files via upload

---
+++ amg.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Main{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        
+        for(int i=0;i<t;i++)
+            {
+                int n=sc.nextInt();
+                int temp=n;
+                int rev=0;
+                while(n>0)
+                {
+                    int dig=n%10;
+                    rev=rev+dig*dig*dig;
+                    n/=10;                   
+                }
+                System.out.println(rev);
+                if(temp==rev)
+                {
+                    System.out.println("amg number");
+                }
+                else
+                {
+                    System.out.println("not amg");
+                }
+            }
+    }
+}
